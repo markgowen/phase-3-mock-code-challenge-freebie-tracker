030c12e6ca4a0da9c36d91959769ab523f1f58bf
Mark <mark.gowen78@gmail.com>
2022-12-22 12:48:47 -0600

    Lab solution

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,12 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
 
+  def give_freebie(dev, freebie)
+    freebie.update(company: self, dev: dev)
+  end
+
+  def self.oldest_company
+    order(:founding_year).first
+  end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,12 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    freebies.any? { |freebie| freebie.item_name == item_name }
+  end
+
+  def give_away(freebie, new_owner)
+    freebie.update(dev: new_owner)
+  end
 end
